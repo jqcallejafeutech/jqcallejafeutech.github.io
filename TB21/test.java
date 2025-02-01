2e26f4626213177d5b8680716009c846d43cd5e4
jqcallejafeutech <148306310+jqcallejafeutech@users.noreply.github.com>
2025-02-01 17:12:33 +0800

    01/02/2025 17:12 Update lesson files.

---
+++ TB21/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String args[]){
+        int a = 6, b = 2, c = 8, y = 4;
+
+        int result = c++ - --y + c / b * a;
+
+        System.out.print(result);
+    }
+}
---
+++ TW23/test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 6, b = 2, c = 8, y = 4;
+
+    int result = c++ - --y + c / b * a;
+
+    cout << result;
+}
