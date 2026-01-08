4b7b8d3fd00a575a99d63e090fe081188e77a1c6
jqcallejafeutech <148306310+jqcallejafeutech@users.noreply.github.com>
2026-01-08 15:11:27 +0800

    Unang push

---
+++ module1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Module 1 Home Page</title>
+</head>
+<body>
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quidem, quia fugiat aliquam incidunt nulla et facilis ipsum, similique, provident corporis culpa velit at voluptas. Laboriosam doloremque cumque voluptatibus autem?</p>
+    </div>
+</body>
+</html>
